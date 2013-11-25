d6854cac64ea26f545caefbd8b61fc991c28aa6a
annic <caoanni@gmail.com>
2013-11-24 23:04:38 -0800

    Add some comments

--- iLearnLetters/GameViewController.m
+++ iLearnLetters/GameViewController.m
@@ -2,7 +2,7 @@
 //  GameViewController.m
 //  iLearnLetters
 //
-//  Created by Zheng Wang on 2013-11-19.
+//  Created by Anni Cao on 2013-11-19.
 //  Copyright (c) 2013 Rouzbeh Roshanravan. All rights reserved.
 //
 

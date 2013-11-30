d88a4a7ee2146801c4fa9366b7026091d615e715
annic <caoanni@gmail.com>
2013-11-29 20:53:29 -0800

    Added standard header.

--- iLearnLetters/GameViewController.h
+++ iLearnLetters/GameViewController.h
@@ -1,9 +1,19 @@
 //
-//  GameViewController.h
-//  iLearnLetters
+//  Purpose: Interface for class GameViewController
 //
-//  Created by Anni Cao on 2013-11-19.
-//  Copyright (c) 2013 Rouzbeh Roshanravan. All rights reserved.
+//  Application: iLearnLetters
+//
+//  Team: Flying Moose - Group 1
+//
+//  Revision history:
+//
+//  Date            Author                      Description
+//  ---------------------------------------------------------------------------
+//  2013-11-19      Anni Cao                    Original definition
+//
+//  Known bugs: N/A
+//
+//  Copyright (c) 2013 Anni Cao. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
